73d493b1cf021f5ef17963f3e9809deade301f7e
Abolarinwa Elegbede <lekan_elegbede@yahoo.com>
2026-08-07 12:22:41 -0400

    COMMIT

---
+++ ML2/L1/Lectures/2/math_operations.py
@@ -0,0 +1,13 @@
+# create this math_operations.py file in your computer at same location as this file
+
+def add_numbers(a, b):
+    """Add two numbers."""
+    return a + b
+
+def subtract_numbers(a, b):
+    """Subtract two numbers."""
+    return a - b
+
+def multiply_numbers(a=1, b=1):
+    """Multiply two numbers with default arguments."""
+    return a * b
